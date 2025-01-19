b4ca23c19102ee4e840c56406213a0cc123e7bc5
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-01-19 01:21:12 -0800

    add d.ts for eslint

---
+++ eslint/index.d.ts
@@ -0,0 +1,9 @@
+declare type Config = {
+    // path/* to ignore directory
+    ignores: string[], 
+    // root dir of tsconfig, likely `import.meta.dirname`
+    tsconfigRootDir: string,
+}
+
+declare function config(opts: Config): any;
+declare function override(opts: Config, override: any): any;
