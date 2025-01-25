8e356305575b1e3be870186cbf9e55ef3aa9f2ee
Pistonight <pistonknight@outlook.com>
2025-01-25 15:38:03 -0800

    run prettier on ourself

--- eslint/index.d.ts
+++ eslint/index.d.ts
@@ -1,9 +1,9 @@
 declare type Config = {
     // path/* to ignore directory
-    ignores: string[], 
+    ignores: string[];
     // root dir of tsconfig, likely `import.meta.dirname`
-    tsconfigRootDir: string,
-}
+    tsconfigRootDir: string;
+};
 
 declare function config(opts: Config): any;
 declare function override(opts: Config, override: any): any;
