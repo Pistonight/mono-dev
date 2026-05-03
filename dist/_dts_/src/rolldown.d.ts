8afe18cf88b6934e1d1cc387112da6127e9bb53f
Pistonight <pistonknight@outlook.com>
2026-05-03 13:18:53 -0700

    publish: 0.4.13

--- dist/_dts_/src/rolldown.d.ts
+++ dist/_dts_/src/rolldown.d.ts
@@ -1,31 +1,6 @@
-export interface PreRenderedChunk {
-    /** The name of this chunk, which is used in naming patterns. */
-    name: string;
-    /** Whether this chunk is a static entry point. */
-    isEntry: boolean;
-    /** Whether this chunk is a dynamic entry point. */
-    isDynamicEntry: boolean;
-    /** The id of a module that this chunk corresponds to. */
-    facadeModuleId?: string;
-    /** The list of ids of modules included in this chunk. */
-    moduleIds: Array<string>;
-    /** Exported variable names from this chunk. */
-    exports: Array<string>;
-}
-export interface PreRenderedAsset {
-    type: "asset";
-    /** @deprecated Use {@linkcode names} instead. */
-    name?: string;
-    names: string[];
-    /** @deprecated Use {@linkcode originalFileNames} instead. */
-    originalFileName?: string;
-    /** The list of the absolute paths to the original file of this asset. */
-    originalFileNames: string[];
-    /** The content of this asset. */
-    source: AssetSource;
-}
-export type AssetSource = string | Uint8Array;
-export declare const wrapChunkFileNames: (original: string | ((chunk: PreRenderedChunk) => string) | undefined, fn: (chunk: PreRenderedChunk) => string | undefined) => ((chunk: PreRenderedChunk) => string);
-export declare const wrapEntryFileNames: (original: string | ((chunk: PreRenderedChunk) => string) | undefined, fn: (chunk: PreRenderedChunk) => string | undefined) => ((chunk: PreRenderedChunk) => string);
-export declare const wrapAssetFileNames: (original: string | ((chunk: PreRenderedAsset) => string) | undefined, fn: (chunk: PreRenderedAsset) => string | undefined) => ((chunk: PreRenderedAsset) => string);
+import type { OutputOptions as RolldownOutputOptions, PreRenderedAsset as RolldownPreRenderedAsset, PreRenderedChunk as RolldownPreRenderedChunk } from "rolldown";
+export type { RolldownOutputOptions, RolldownPreRenderedChunk, RolldownPreRenderedAsset };
+export declare const wrapChunkFileNames: (isLib: boolean, original: string | ((chunk: RolldownPreRenderedChunk) => string) | undefined, fn: (chunk: RolldownPreRenderedChunk) => string | undefined) => ((chunk: RolldownPreRenderedChunk) => string);
+export declare const wrapEntryFileNames: (isLib: boolean, original: string | ((chunk: RolldownPreRenderedChunk) => string) | undefined, fn: (chunk: RolldownPreRenderedChunk) => string | undefined) => ((chunk: RolldownPreRenderedChunk) => string);
+export declare const wrapAssetFileNames: (original: string | ((chunk: RolldownPreRenderedAsset) => string) | undefined, fn: (chunk: RolldownPreRenderedAsset) => string | undefined) => ((chunk: RolldownPreRenderedAsset) => string);
 //# sourceMappingURL=rolldown.d.ts.map
--- dist/rolldown.js
+++ dist/rolldown.js
@@ -1,5 +1,5 @@
 //#region src/rolldown.ts
-var e = (e, t) => (n) => t(n) || (e ? typeof e == "string" ? e : e(n) : "[name]-[hash].js"), t = (e, t) => (n) => t(n) || (e ? typeof e == "string" ? e : e(n) : "[name].js"), n = (e, t) => (n) => t(n) || (e ? typeof e == "string" ? e : e(n) : "assets/[name]-[hash][extname]");
+var e = (e, t, n) => (r) => n(r) || (t ? typeof t == "string" ? t : t(r) : e ? "[name]-[hash].js" : "assets/[name]-[hash].js"), t = (e, t, n) => (r) => n(r) || (t ? typeof t == "string" ? t : t(r) : e ? "[name].js" : "assets/[name]-[hash].js"), n = (e, t) => (n) => t(n) || (e ? typeof e == "string" ? e : e(n) : "assets/[name]-[hash][extname]");
 //#endregion
 export { n as wrapAssetFileNames, e as wrapChunkFileNames, t as wrapEntryFileNames };
 
