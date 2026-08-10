7a35976e04c823176a1381557bc46935e4527eca
Pistonight <pistonknight@outlook.com>
2026-08-09 18:44:37 -0700

    publish: 0.4.26

--- dist/cli/index.js
+++ dist/cli/index.js
@@ -346,7 +346,7 @@ var T = (e) => {
 		let n = e[t];
 		if (typeof n != "string") continue;
 		let [r, i] = n.split("#", 2), a = r.toLowerCase();
-		!a.startsWith("github:") || !a.endsWith("/mono-dev") || (e[t] = r + "#7952165fe7b8eef764b794b4443924017c60c941");
+		!a.startsWith("github:") || !a.endsWith("/mono-dev") || (e[t] = r + "#650a84fcce97288d81b3f0a6e28ee0e70ef7a8a8");
 	}
 }, B = {
 	"install-cargo-extra-tools": "Install or upgrade extra tools needed for development using cargo onto the system",
